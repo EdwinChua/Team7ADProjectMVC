e779807f48af944e63ef8e918f0e367ef49a8125
Edwin <3dw1nchu4@gmail.com>
2017-01-16 13:11:48 +0800

    initial commit

---
+++ Team7ADProjectMVC/Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team7ADProjectMVC.TestControllers
+{
+    public class StoreController : Controller
+    {
+        ProjectEntities db = new ProjectEntities();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View("Dashboard");
+        }
+
+        public ActionResult Inventory()
+        {
+            var inventories = db.Inventories;
+            var categories = db.Categories;
+            ViewBag.Cat = categories.ToList();
+            return View("ViewInventory",inventories.ToList());
+        }
+
+        public ActionResult InventoryItem(String id)
+        {
+            Inventory inventory = db.Inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.inv = inventory;
+            return View("ViewStockCard",inventory);
+        }
+    }
+}
---
+++ Team7ADProjectMVC/TestControllers/RepresentativeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team7ADProjectMVC.TestControllers
+{
+    public class RepresentativeController : Controller
+    {
+        // GET: Representative
+        public ActionResult Index()
+        {
+            return View("ViewRequisitionDetails");
+        }
+
+        public ActionResult Test()
+        {
+            return Index();
+        }
+    }
+}

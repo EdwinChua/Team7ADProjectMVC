acfeec238a29e21b3b386352fdadd4b358ebb8d5
Edwin <3dw1nchu4@gmail.com>
2017-01-17 12:12:29 +0800

    coomit

--- Team7ADProjectMVC/Models/DisbursementService/DisbursementService.cs
+++ Team7ADProjectMVC/Models/DisbursementService/DisbursementService.cs
@@ -9,11 +9,13 @@ namespace Team7ADProjectMVC.Models
     {
         public List<DisbursementList> GetAllDisbursements()
         {
+            //TODO: Linda 
             throw new NotImplementedException();
         }
 
         public List<DisbursementList> GetDisbursementsBySearchCriteria(Department department, string status)
         {
+            //TODO: Linda 
             throw new NotImplementedException();
         }
     }

dde9bbdbf4a4edaa2d462b27fcf2b3b203cf3dcd
linda <e0046727@u.nus.edu>
2017-01-17 12:32:06 +0800

    linda

--- Team7ADProjectMVC/Models/DisbursementService/DisbursementService.cs
+++ Team7ADProjectMVC/Models/DisbursementService/DisbursementService.cs
@@ -7,15 +7,19 @@ namespace Team7ADProjectMVC.Models
 {
     public class DisbursementService : IDisbursementService
     {
+        ProjectEntities db = new ProjectEntities();
         public List<DisbursementList> GetAllDisbursements()
         {
             //TODO: Linda 
+            var disbursementList = db.DisbursementLists;
+            return (disbursementList.ToList());
             throw new NotImplementedException();
         }
 
         public List<DisbursementList> GetDisbursementsBySearchCriteria(Department department, string status)
         {
             //TODO: Linda 
+            
             throw new NotImplementedException();
         }
     }

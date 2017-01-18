ad6299d781393e949fb8ec57a0c1fb21b742359b
Edwin <3dw1nchu4@gmail.com>
2017-01-18 14:32:15 +0800

    minor db changes

--- Team7ADProjectMVC/database.sql
+++ Team7ADProjectMVC/database.sql
@@ -6,7 +6,7 @@ DROP DATABASE Project
 
 CREATE DATABASE Project
 
----------------------------------------- Tne run this till the end to create tables -----------------------------------------
+-------------------------------------- Tne run this till the end to create tables -----------------------------------------
 USE Project
 
 --------------------------------------- Supplier -----------------------------------------
@@ -206,15 +206,15 @@ CONSTRAINT RequisitionEmployeeId FOREIGN KEY(EmployeeId) REFERENCES Employee(Emp
 )
 
 INSERT INTO Requisition
-VALUES (1,4,8,'2017-01-03','2017-01-03','Approved')
+VALUES (1,4,8,'2017-01-03','2017-01-03','Approved','')
 INSERT INTO Requisition
-VALUES (1,4,15,'2017-01-01','2017-01-01','Approved')
+VALUES (1,4,15,'2017-01-01','2017-01-01','Approved','')
 INSERT INTO Requisition
-VALUES (1,4,15,'2017-01-04','2017-01-04','Approved')
+VALUES (1,4,15,'2017-01-04','2017-01-04','Approved','')
 INSERT INTO Requisition
-VALUES (1,4,1,'','2017-01-14','Pending')
+VALUES (1,4,1,'','2017-01-14','Pending','')
 INSERT INTO Requisition
-VALUES (1,4,15,'','2017-01-15','Pending')
+VALUES (1,4,15,'','2017-01-15','Pending','')
 
 
 -------------------------------------------------- Category ----------------------------------------
@@ -417,7 +417,7 @@ CREATE TABLE DisbursementDetail
 DisbursementDetailId INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 DisbursementListId INT,
 RequisitionDetailId INT,
-OrderedQuantity INT,
+PreparedQuantity INT,
 DeliveredQuantity INT,
 Remark VARCHAR(250),
 CONSTRAINT DisbursementListId FOREIGN KEY (DisbursementListId) REFERENCES DisbursementList (DisbursementListId),
@@ -474,7 +474,7 @@ INNER JOIN Employee e on e.EmployeeId = a.EmployeeId
 INNER JOIN Inventory i on i.ItemNo = ad.ItemNo
 
 UNION
-SELECT ISNULL(dl.DeliveryDate,'1900-01-01') AS [Date], d.DepartmentName , '-' + CAST(dd.Quantity AS VARCHAR(100)) As DeliveredQuantity, rd.ItemNo, i.Description FROM DisbursementDetail dd
+SELECT ISNULL(dl.DeliveryDate,'1900-01-01') AS [Date], d.DepartmentName , '-' + CAST(dd.PreparedQuantity AS VARCHAR(100)) As DeliveredQuantity, rd.ItemNo, i.Description FROM DisbursementDetail dd
 LEFT JOIN RequisitionDetail rd on rd.RequisitionId = dd.RequisitionDetailId
 INNER JOIN DisbursementList dl on dl.DisbursementListId = dd.DisbursementListId
 INNER JOIN Department d on d.DepartmentId = dl.DepartmentId
@@ -489,7 +489,7 @@ INNER JOIN Supplier s on s.SupplierId = po.SupplierId
 
 -------------------------------------- CrystalReports Views ----------------------------------------
 create view disbAnalysis as
-select d.DepartmentName, c.CategoryName, r.ApprovedDate, dd.Quantity, e.EmployeeName, r.RequisitionId
+select d.DepartmentName, c.CategoryName, r.ApprovedDate, dd.PreparedQuantity, e.EmployeeName, r.RequisitionId
 from 
 DisbursementDetail dd, 
 RequisitionDetail rd, 

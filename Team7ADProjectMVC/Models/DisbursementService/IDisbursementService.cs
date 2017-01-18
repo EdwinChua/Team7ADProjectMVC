a2558742c6fc336bdbe30503160e51e9f61caa73
sanyuehuakui <410545629@QQ.com>
2017-01-18 16:24:35 +0800

    Rep-viewdisbursementlist

--- Team7ADProjectMVC/Models/DisbursementService/IDisbursementService.cs
+++ Team7ADProjectMVC/Models/DisbursementService/IDisbursementService.cs
@@ -8,6 +8,7 @@ namespace Team7ADProjectMVC.Models
     interface IDisbursementService
     {
         List<DisbursementList> GetAllDisbursements();
+        List<DisbursementList> GetdisbursementsByStatus(string status);
 
         List<DisbursementList> GetDisbursementsBySearchCriteria(int? departmentId, String status);
         //Search for disbursements by department and status
@@ -15,5 +16,7 @@ namespace Team7ADProjectMVC.Models
         DisbursementList GetDisbursementById(String id);
 
         void UpdateDisbursementList(DisbursementList disbursementList);
+
+        List<DisbursementList> GetdisbursementsByDept(int? id);
     }
 }

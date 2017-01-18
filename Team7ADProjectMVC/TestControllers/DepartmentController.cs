d12d3e6a02710380c4b10dcd014828d4805bbd57
e0046486 <e0046486@ISSBOR3807>
2017-01-18 14:12:12 +0800

    me

--- Team7ADProjectMVC/TestControllers/DepartmentController.cs
+++ Team7ADProjectMVC/TestControllers/DepartmentController.cs
@@ -22,23 +22,60 @@ namespace Team7ADProjectMVC.TestControllers
             return View();
         }
 
+        // GET: TESTRequisitions/Create
         public ActionResult MakeRequisition()
         {
-            var requisitions = db.Requisitions.ToList();
-            ViewBag.Cat = requisitions;
+            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "EmployeeName");
 
+            List<RequisitionDetail> relis = db.RequisitionDetails.Take(3).ToList();
             
 
-            //List<RequisitionDetail> relis = db.RequisitionDetails.ToList();
+            ViewBag.MembershipList = db.Inventories.ToList();
 
-            //ViewBag.rel = relis;
+            //var query = from t in db.Inventories
+            //            where t.Category.CategoryName.Equals("Clips")
+            //            select t;
 
+            ViewBag.clips = db.Inventories.Where(x => x.Category.CategoryName == "Clips").ToList();
+            //ViewBag.clips = query.ToList();
+
+
+            ViewBag.rel = relis;
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MakeRequisition([Bind(Include = "RequisitionId,EmployeeId,DepartmentId,ApprovedBy,ApprovedDate,OrderedDate,RequisitionStatus")] Requisition requisition)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Requisitions.Add(requisition);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             List<RequisitionDetail> relis = db.RequisitionDetails.Take(3).ToList();
 
             ViewBag.rel = relis;
 
+            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "EmployeeName", requisition.EmployeeId);
+            return View(requisition);
+            //var requisitions = db.Requisitions.ToList();
+            //ViewBag.Cat = requisitions;
 
-            return View(requisitions);
+
+
+            ////List<RequisitionDetail> relis = db.RequisitionDetails.ToList();
+
+            ////ViewBag.rel = relis;
+
+           
+
+            //return View(requisitions);
         }
 
         // GET: TESTRequisitions/Details/5

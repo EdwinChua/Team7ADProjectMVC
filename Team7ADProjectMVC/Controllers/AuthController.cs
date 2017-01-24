bdc5706b36e40ae83a2f74513e58ebc3128cd9f0
zhanseng <sumzhanseng@hotmail.com>
2017-01-24 17:00:41 +0800

    authentication up

---
+++ Team7ADProjectMVC/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team7ADProjectMVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        ProjectEntities db = new ProjectEntities();
+        // GET: Auth
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                int userId = Int32.Parse(User.Identity.Name);
+                Session["user"] = db.Employees.Find(userId);
+                return Content(((Employee)Session["User"]).EmployeeName);
+            }
+
+            else return Content("not authenticated");
+            
+        }
+    }
+}
---
+++ Team7ADProjectMVC/CreateUserWizard.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateUserWizard.aspx.cs" Inherits="Team7ADProjectMVC.CreateUserWizard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:CreateUserWizard ID="CreateUserWizard1" runat="server">
+            <WizardSteps>
+                <asp:CreateUserWizardStep runat="server" />
+                <asp:CompleteWizardStep runat="server" />
+            </WizardSteps>
+        </asp:CreateUserWizard>
+    </form>
+</body>
+</html>

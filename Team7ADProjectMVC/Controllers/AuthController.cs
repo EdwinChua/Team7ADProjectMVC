6f3c9c872e2b423436c72c847d69907ac04ea4a6
zhanseng <sumzhanseng@hotmail.com>
2017-01-25 11:28:07 +0800

    android authentication solution

--- Team7ADProjectMVC/Controllers/AuthController.cs
+++ Team7ADProjectMVC/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Team7ADProjectMVC.Controllers
 {
@@ -18,9 +19,22 @@ namespace Team7ADProjectMVC.Controllers
                 Session["user"] = db.Employees.Find(userId);
                 return Content(((Employee)Session["User"]).EmployeeName);
             }
-
+            
             else return Content("not authenticated");
             
+        }
+
+        [HttpPost]
+        public ActionResult AndroidAuth(FormCollection f)
+        {
+            if (Membership.ValidateUser(Request.Form["1"], Request.Form["2"]))
+            {
+                int userId = Int32.Parse(Request.Form["1"]);
+
+                return Content(db.Employees.Find(userId).EmployeeName);
+            }
+
+            return Content("failed");
         }
     }
 }

185c875ddff3ff33df3ab2c5b64e9b0438afa4b0
zhanseng <sumzhanseng@hotmail.com>
2017-01-27 12:34:12 +0800

    reports outstanding: layering and validation bugs, but fully functional

--- Team7ADProjectMVC/ReportViewer.aspx.cs
+++ Team7ADProjectMVC/ReportViewer.aspx.cs
@@ -16,6 +16,11 @@ namespace Team7ADProjectMVC
             //DataSet1.PurchaseAnalysisDataTable dt = new DataSet1.PurchaseAnalysisDataTable();
             //DataSet1TableAdapters.PurchaseAnalysisTableAdapter da = new DataSet1TableAdapters.PurchaseAnalysisTableAdapter();
             //da.Fill(dt);
+            //ReportDocument cr = new ReportDocument();
+            //cr.Load(Server.MapPath("~/Reports/CrystalReport2.rpt"));
+            //cr.SetDataSource((DataTable)dt);
+            //CrystalReportViewer1.ReportSource = cr;
+           
             ReportDocument cr = new ReportDocument();
             cr.Load(Server.MapPath(Session["Path"].ToString()));
             cr.SetDataSource(Session["data"]);
